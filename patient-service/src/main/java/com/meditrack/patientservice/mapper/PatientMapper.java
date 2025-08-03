3ea41f34aa091220cdc08035946d2bc38b5a888e
guptaRishitaa <rishitagupta2700@gmail.com>
2025-08-03 17:51:50 -0400

    Patient service

---
+++ patient-service/src/main/java/com/meditrack/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,17 @@
+package com.meditrack.patientservice.mapper;
+
+import com.meditrack.patientservice.dto.PatientResponseDTO;
+import com.meditrack.patientservice.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+}
---
+++ patient-service/src/main/java/com/meditrack/patientservice/service/PatientService.java
@@ -0,0 +1,28 @@
+package com.meditrack.patientservice.service;
+
+import com.meditrack.patientservice.dto.PatientResponseDTO;
+import com.meditrack.patientservice.mapper.PatientMapper;
+import com.meditrack.patientservice.model.Patient;
+import com.meditrack.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream().map(PatientMapper::toDTO).toList();
+
+        return patientResponseDTOs;
+
+
+    }
+}
